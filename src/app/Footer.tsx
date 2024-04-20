2fbfa60ee9d46c23f6aa9ffaf3b6d30c57f44e49
shreyas710 <shreyasnk5321@gmail.com>
2024-04-19 19:16:39 -0700

    lazygit

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,9 +1,12 @@
 export default function Footer() {
-    return (
-        <footer className="footer mt-auto py-3 bg-dark">
-            <div className="container text-center">
-                <span className="text-light">Copyright &copy; <span>{new Date().getFullYear()}</span> by Shreyas Kulkarni</span>
-            </div>
-        </footer>
-    )
+  return (
+    <footer className="mt-auto py-3 bg-dark">
+      <div className="container text-center">
+        <span className="text-light">
+          Copyright &copy; <span>{new Date().getFullYear()}</span> by Shreyas
+          Kulkarni
+        </span>
+      </div>
+    </footer>
+  );
 }
